74d5adb0e29a836daa69f28d6a332883ad666fcc
Andrew Benton <andrewmbenton@gmail.com>
2011-02-16 13:07:04 -0800

    fix namespacing bug in available_phone_numbers/toll-free

--- lib/twilio-ruby/rest/available_phone_numbers/toll_free.rb
+++ lib/twilio-ruby/rest/available_phone_numbers/toll_free.rb
@@ -3,7 +3,7 @@ module Twilio
     class TollFree < ListResource
       def initialize(uri, client)
         @resource_name = 'available_phone_numbers'
-        @instance_class = Twilio.const_get 'AvailablePhoneNumber'
+        @instance_class = Twilio::REST.const_get 'AvailablePhoneNumber'
         @uri, @client = uri, client
       end
     end
