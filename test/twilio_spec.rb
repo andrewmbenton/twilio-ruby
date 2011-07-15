a47c49ac938da37d1741b8dad42057741b3969b4
Andrew Benton <andrewmbenton@gmail.com>
2011-07-15 12:49:44 -0700

    a couple more tests

--- test/twilio_spec.rb
+++ test/twilio_spec.rb
@@ -71,6 +71,20 @@ describe Twilio::REST::Client do
   end
 end
 
+describe Twilio::REST::InstanceResource do
+  it 'should set up an internal reference to the uri and client' do
+    resource = Twilio::REST::InstanceResource.new('some/uri', 'someClient')
+    resource.instance_variable_get('@uri').should == 'some/uri'
+    resource.instance_variable_get('@client').should == 'someClient'
+  end
+
+  it 'should set up object properties if passed' do
+    params = {'SomeKey' => 'someValue'}
+    resource = Twilio::REST::InstanceResource.new('uri', 'client', params)
+    resource.some_key.should == 'someValue'
+  end
+end
+
 describe Twilio::REST::Account do
   it 'should set up an incoming phone numbers resources object' do
     account = Twilio::REST::Account.new('someUri', 'someClient')
